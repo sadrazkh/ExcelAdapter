310aacd4be33913c3db26ec73bb52169e2357298
sadra zadeh khameneh <sadra.z.kh@gmail.com>
2021-06-20 02:37:17 +0430

    init SimpleExcelAdapter

--- ExcelAdaptor/Simple.cs
+++ ExcelAdaptor/Simple.cs
@@ -3,11 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
 
 namespace ExcelAdapter
 {
-    public class Simple
+    public class SimpleExcelAdapter
     {
+        private readonly string path;
+        private _Application excel = new _Excel.Application();
+        private Workbook wb;
+        private Worksheet ws;
 
+        public SimpleExcelAdapter(string path, int sheet)
+        {
+            this.path = path;
+            wb = excel.Workbooks.Open(path);
+            ws = wb.Worksheets[sheet];
+        }
+
+        public string ReadCell(int i, int j)
+        {
+            i += 1;
+            j += 1;
+            if (ws.Cells[i,j].Value2 != null)
+            {
+                return ws.Cells[i, j].Value2;
+            }
+            else
+            {
+                return "";
+            }
+        }
     }
 }
